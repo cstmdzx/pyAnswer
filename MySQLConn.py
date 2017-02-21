51dcbf627055599c2dcb197911608ade5486a272
root <root@localhost.localdomain>
2017-02-21 12:12:31 +0800

    添加了一个新的函数

--- MySQLConn.py
+++ MySQLConn.py
@@ -5,9 +5,15 @@
 import MySQLdb
 
 def mysql_conn():
-    conn = MySQLdb.connect(host='localhost', user='root', passwd='Dbis_23508468', db='dbpd_useid')
+    conn = MySQLdb.connect(host='localhost', user='root', passwd='Dbis_23508468', db='dbpd')
     cur = conn.cursor()
     return cur
+
+def mysql_conn_name(str_name):
+    conn = MySQLdb.connect(host='localhost', user='root',passwd='Dbis_23508468',db=str_name)
+    cur = conn.cursor()
+    return cur
+
 
 if __name__ == '__main__':
     curr = mysql_conn()
