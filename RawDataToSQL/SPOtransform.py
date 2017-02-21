fe118ece484b8fa8f5532dd0701fe26e9154d6c5
root <root@localhost.localdomain>
2017-02-21 23:08:45 +0800

    添加了一个用来转换三元组的函数，主要用来生成各种数据

---
+++ RawDataToSQL/SPOtransform.py
@@ -0,0 +1,48 @@
+# -*-coding:UTF-8-*-
+# 先生成一个O_SP试试，不行再想辙，行了就接着这条路走下去
+
+import time
+
+if __name__ == '__main__':
+    print 'Start Reading File:'
+    start = time.clock()
+    fileSPO = open('./resSPO/SPO')
+    linesSPO = fileSPO.readlines()
+
+
+    print 'Finish Reading, Time: %f' % (time.clock() - start)
+    fileS_OP = open('./resSPO/S_OP', 'w')
+    fileP_SO = open('./resSPO/P_SO', 'w')
+    fileO_SP = open('./resSPO/O_SP', 'w')
+    fileOP_S = open('./resSPO/OP_S', 'w')
+    fileSO_P = open('./resSPO/SO_P', 'w')
+    fileSP_O = open('./resSPO/SP_O', 'w')
+
+    intCount = 0
+    for eachSPO in linesSPO:
+        intCount += 1
+        if intCount % 10000000 == 0: # total 250000000
+            print time.clock() - start
+        words = eachSPO.split('\t')
+        # print words
+        strSub = words[0]
+        strPred = words[1]
+        strObj = words[2]
+        if words.__len__() < 3:
+            continue
+        # fileS_OP.write(strSub + '\t' + strObj + '\t' + strPred + '\n')
+        # fileP_SO.write(strPred + '\t' + strSub + '\t' + strObj + '\n')
+        fileO_SP.write(strObj + '\t' + strSub + '\t' + strPred + '\n')
+        # fileSO_P.write(strSub + '\t' + strObj + '\t' + strPred + '\n')
+        # fileSP_O.write(strSub + '\t' + strPred + '\t' + strObj + '\n')
+        # fileOP_S.write(strObj + '\t' + strPred + '\t' + strSub + '\n')
+
+    print 'Finish Writing, Time: %f' % (time.clock() - start)
+    fileS_OP.close()
+    fileP_SO.close()
+    fileO_SP.close()
+    fileOP_S.close()
+    fileSO_P.close()
+    fileSP_O.close()
+    print 'Finish Closing, Time: %f' % (time.clock() - start)
+
