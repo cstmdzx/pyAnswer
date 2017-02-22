28b7bf89426edbff814e3e0976f8b23f57386797
root <root@localhost.localdomain>
2017-02-22 23:35:06 +0800

    一些Lib，目前只有一个用来连接virtuoso的，下次把mysql的也放进来

---
+++ Lib/VirtuosoLib.py
@@ -0,0 +1,20 @@
+# -*-coding:UTF-8-*-
+
+# 在这里写很多virtuoso的函数
+
+import re
+
+def query_sparql(str_sparql_cmd):
+    # 执行sparql查询，
+    # 输入为一条sparql语句，返回为json的查询结果
+    # unfinished yet
+
+def json_to_list(json_query_res):
+    # 处理查询返回的json表达式
+    # 输入为查询之后的json，输出为将json处理后的list ，
+    # 应该会用到正则表达式
+    # unfinished yet
+
+if __name__ == '__main__':
+    # test
+
