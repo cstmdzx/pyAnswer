5d144bd1678ce72db05a190b5c0d2ece9cfcf56c
cstmdzx <cstmdzx@163.com>
2017-03-01 14:41:40 +0800

    修改成用virtuoso来处理的

--- PathSupportSet/PathSupportSet.py
+++ PathSupportSet/PathSupportSet.py
@@ -6,6 +6,8 @@
 
 import MySQLdb
 import re
+import sys
+import time
 
 from GetEntityPairByPath import get_entity_pair_by_path
 
@@ -17,30 +19,51 @@ cur = mysql_conn()
 '''
 
 if __name__ == '__main__':
-    filePredPathSupSet = open('FilePredPathSupSet', 'w') # 保存结果词对
-    filePredPath = open('FilePredicatePath', 'r') # 读取之前的结果, 这里保存的之前的候选路径
-    filePredPathAssociateRep = open('FilePredPathAssociateRep', 'w') #
+    strFilePathRes = './PathRes/'
+    strFileAssociateRes = './AssociateRes/'
+
+    start = time.time()
+    filePredPathSupSet = open(strFilePathRes + 'FilePredPathSupSetLen1', 'w') # 保存结果词对
+    filePredPath = open('../CandidatePredicatePath/FilePredicatePath/FileLen1Merged', 'r') # 读取之前的结果, 这里保存的之前的候选路径
+    filePredPathAssociateRep = open(strFileAssociateRes + 'FilePredPathAssociateRepLen1', 'w') #
 
     linesPredPath = filePredPath.readlines()
     dictPredPathAssociateRep = dict()
 
+    print 'File Read Finish, Time: %f' % (time.time() - start)
+    intFlag = 0
+    intExist = 0
     for eachRepIdPredPath in linesPredPath:
+        intFlag += 1
+        if intFlag % 1000 == 0:
+            # print intExist
+            # intExist = 0
+            print intFlag
+            print time.time() - start
         eachRepIdPredPath = eachRepIdPredPath.replace('\n', '')
         words = eachRepIdPredPath.split('~')
         strRepId = words[0]
         del words[0] # 去掉RepId,剩下的全是path
 
         for eachPath in words:
-            filePredPathSupSet.write(eachPath)
             if eachPath in dictPredPathAssociateRep:
                 dictPredPathAssociateRep[eachPath] += '~' + strRepId
+                # intExist += 1
                 continue # 查出过匹配词对的不再继续去匹配，结果应该也写在硬盘上了
             else:
                 dictPredPathAssociateRep[eachPath] = strRepId
 
-            listEntityPair = get_entity_pair_by_path(eachPath)
+            filePredPathSupSet.write(eachPath)
+            try:
+                listEntityPair = get_entity_pair_by_path(eachPath)
+            except Exception as e:
+                print e
+                print eachPath
+                continue
+            except KeyboardInterrupt:
+                sys.exit(0)
             for eachEntityPair in listEntityPair:
-                filePredPathSupSet.write('~' + eachEntityPair[0] + '\t' + eachEntityPair[1])
+                filePredPathSupSet.write(('~' + eachEntityPair['sl'] + '\t' + eachEntityPair['ol']).encode('UTF-8'))
             filePredPathSupSet.write('\n')
 
     # 把与每个PredPath有关的RepId保存下来，后面计算相似度
