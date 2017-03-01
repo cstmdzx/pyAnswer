f938e756455b9df7ed8cf853400791e0015cab81
cstmdzx <cstmdzx@163.com>
2017-03-01 14:42:36 +0800

    修改成用virtuoso来处理的

--- PathSupportSet/GetEntityPairByPath.py
+++ PathSupportSet/GetEntityPairByPath.py
@@ -15,28 +15,32 @@ cur = mysql_conn_name('dbpd_spo')
 
 import sys
 sys.path.append('..')
-from Lib.VirtuosoLib import query_sparql
-from Lib.VirtuosoLib import json_to_list
+#from Lib.VirtuosoLib import query_sparql
+#from Lib.VirtuosoLib import json_to_list
+from Lib.FuncVirtuosoConn import get_query_results
+
 
 def get_entity_pair_by_path(str_path):
     # 根据谓语路径，产生对应的词对
     # 输入为每个谓语路径，返回结果为对应词对的list
     # 这里没有记录每个path的对应的candidate，调用之前要搞一个
-    words_pred_with_dir = str_path.split(']')
     list_sql_cmd = list()
     list_direct = list()
+    words_pred_with_dir = str_path.split(']')
     int_temp_num = 0
 
     # shengcheng sparql
-    str_param_previous = '?x1'
-    str_sparql_prefix = 'select ?x1,'
-    str_sparql_cmd = 'where { '
+    str_param_previous = '?x0'
+    #str_sparql_prefix = 'select ?sl,?ol '
+    str_sparql_cmd = 'select ?sl,?ol where { '
+    str_sparql_cmd += '?x0 <http://www.w3.org/2000/01/rdf-schema#label> ?sl . '
     for each_pred_with_dir in words_pred_with_dir:
         int_temp_num += 1
         if each_pred_with_dir == '': # zui hou yi ge ke neng shi kong de
             continue
         words = each_pred_with_dir.split('[')
         str_pred = words[0]
+        str_pred = '<' + str_pred + '>'
         str_direct = words[1]
         # print str_pred
         # print str_direct
@@ -45,12 +49,17 @@ def get_entity_pair_by_path(str_path):
         else:
             str_sparql_cmd += '?x' + str(int_temp_num) + ' ' + str_pred + ' ' + str_param_previous + '. '
         str_param_previous = '?x' + str(int_temp_num)
-    str_sparql_final = str_sparql_prefix + str_param_previous + ' ' + str_sparql_cmd + '}'
+    str_sparql_cmd += str_param_previous + ' <http://www.w3.org/2000/01/rdf-schema#label> ?ol . }'
+    str_sparql_final = str_sparql_cmd
+    # print str_sparql_final
 
     # exec
     # 两个待开发的函数，
-    json_query_res = query_sparql(str_sparql_final) # sparql 查询, 返回的是json结果
-    list_res = json_to_list(json_query_res) # json结果转换为list
+    # json_query_res = query_sparql(str_sparql_final) # sparql 查询, 返回的是json结果
+    # list_res = json_to_list(json_query_res) # json结果转换为list
+
+    list_res = get_query_results(str_sparql_final)
+    #list_res = list()
     return list_res
 
     # print str_sparql_final
@@ -119,6 +128,8 @@ def get_entity_pair_by_path(str_path):
 
 if __name__ == '__main__':
 
-    get_entity_pair_by_path('pred1[f]pred2[b]pred3[f]pred4[b]')
+    #get_entity_pair_by_path('pred1[f]pred2[b]pred3[f]pred4[b]')
+    listRes = get_entity_pair_by_path('http://dbpedia.org/property/placeofburial[f]')
+    print listRes
 
 
